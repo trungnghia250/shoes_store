b94d80cde92f440cbbbfb2cac9473020f3364c47
nghiaht <nghiaht@ahamove.com>
2021-11-04 10:36:56 +0700

    update CORS

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/trungnghia250/shoes_store/db"
 	"github.com/trungnghia250/shoes_store/router"
 	"log"
@@ -17,6 +18,8 @@ func main() {
 
 	app := fiber.New()
 	router.Create(app)
+
+	app.Use(cors.New())
 	port := os.Getenv("PORT")
 
 	_ = app.Listen(":"+port)

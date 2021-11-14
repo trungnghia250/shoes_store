0737da664708dccf7b3ff1052492c4ea28eef107
nghiaht <nghiaht@ahamove.com>
2021-11-14 10:19:31 +0700

    update items in order

--- model/order.go
+++ model/order.go
@@ -5,10 +5,17 @@ type Order struct {
 	State     string `json:"state,omitempty" bson:"state,omitempty"`
 	UserId    int32  `json:"user_id,omitempty" bson:"user_id,omitempty"`
 	Detail    string `json:"detail,omitempty" bson:"detail,omitempty"`
+	Items     []Item `json:"items,omitempty" bson:"items,omitempty"`
 	Total     int32  `json:"total,omitempty" bson:"total,omitempty"`
 	OrderDate string `json:"order_date,omitempty" bson:"order_date,omitempty"'`
 }
 
+type Item struct {
+	ID    int32  `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name  string `json:"name,omitempty" bson:"name,omitempty"`
+	Price int32  `json:"price,omitempty" bson:"price,omitempty"`
+	Num   int32  `json:"num,omitempty" bson:"num,omitempty"`
+}
 type GetOrderByUserIDRequest struct {
 	ID int32 `json:"id"`
 }

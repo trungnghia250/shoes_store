9275b12d2b089e1a44b7761bea8130e67cfb7f49
trungnghia250 <trungnghia250>
2021-11-21 16:57:13 +0700

    add field payment_method

--- model/order.go
+++ model/order.go
@@ -1,13 +1,14 @@
 package model
 
 type Order struct {
-	ID        int32  `json:"_id,omitempty" bson:"_id,omitempty"`
-	State     string `json:"state,omitempty" bson:"state,omitempty"`
-	UserId    int32  `json:"user_id,omitempty" bson:"user_id,omitempty"`
-	Detail    string `json:"detail,omitempty" bson:"detail,omitempty"`
-	Items     []Item `json:"items,omitempty" bson:"items,omitempty"`
-	Total     int32  `json:"total,omitempty" bson:"total,omitempty"`
-	OrderDate string `json:"order_date,omitempty" bson:"order_date,omitempty"'`
+	ID            int32  `json:"_id,omitempty" bson:"_id,omitempty"`
+	State         string `json:"state,omitempty" bson:"state,omitempty"`
+	UserId        int32  `json:"user_id,omitempty" bson:"user_id,omitempty"`
+	Detail        string `json:"detail,omitempty" bson:"detail,omitempty"`
+	Items         []Item `json:"items,omitempty" bson:"items,omitempty"`
+	Total         int32  `json:"total,omitempty" bson:"total,omitempty"`
+	OrderDate     string `json:"order_date,omitempty" bson:"order_date,omitempty"`
+	PaymentMethod string `json:"payment_method,omitempty" bson:"payment_method,omitempty"`
 }
 
 type Item struct {
